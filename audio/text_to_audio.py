7b23d2b2cf0d595b29862847d414e6b9a2316791
Sharoon Rafeek <sharoonrafeek@gmail.com>
2021-10-23 17:55:25 +0530

    code organised

--- audio/text_to_audio.py
+++ audio/text_to_audio.py
@@ -1,13 +1,6 @@
-import os
-from dotenv import load_dotenv
-from joke_api.joke import joke
 from gtts import gTTS
 
-load_dotenv()
-url = os.getenv("JOKE_URL")
-text = joke(url)
 
-
-def text_to_speech():
-    audio = gTTS(text=text, slow=True, lang="en")
+def text_to_speech(joke):
+    audio = gTTS(text=joke, slow=True, lang="en")
     audio.save("audio/audio.mp3")
--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import os
+from dotenv import load_dotenv
+from joke_api.joke import joke
 from audio.text_to_audio import text_to_speech
 
-text_to_speech()
+
+load_dotenv()
+
+url = os.getenv("JOKE_URL")
+text = joke(url)
+
+text_to_speech(text)
