4887ab42873bdd365d9121ff0bbe747220a85eb1
Sharoon Rafeek <sharoonrafeek@gmail.com>
2021-10-24 12:30:22 +0530

    image name changes in every download

--- image/image.py
+++ image/image.py
@@ -1,9 +1,21 @@
+import os
 from google_images_download import google_images_download
 
 response = google_images_download.googleimagesdownload()
 
 
 def download_image(joke):
+    assets = os.listdir("assets")
+    for asset in assets:
+        if asset[-4:] == ".png":
+            os.remove(f"assets/{asset}")
+            break
+
     arguments = {"keywords": joke, "limit": 1,
                  "print_urls": False, "no_directory": "true", "format": "png", "output_directory": "assets"}
     response.download(arguments)
+
+    assets = os.listdir("assets")
+    for asset in assets:
+        if asset[-4:] == ".png":
+            os.rename(f"assets/{asset}", "assets/image.png")
--- main.py
+++ main.py
@@ -11,17 +11,17 @@ load_dotenv()
 url = os.getenv("JOKE_URL")
 text = joke(url)
 
-# text_to_speech(text)
+text_to_speech(text)
 new_text = ""
 for char in text:
     if char != ",":
         new_text += char
 
 
-# download_image(new_text)
+download_image(new_text)
 
 audio_path = "assets/audio.mp3"
-image_path = "assets/1.made-a-graph-of-my-past-relationships-i-have-an-58467322.png"
+image_path = "assets/image.png"
 output_path = "video/video.mp4"
 
 add_static_image_to_audio(image_path, audio_path, output_path)
