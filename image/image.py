e44768194614302a58e1ba17e9e416f7f721568f
Sharoon Rafeek <sharoonrafeek@gmail.com>
2021-10-27 11:39:20 +0530

    video modification

--- image/image.py
+++ image/image.py
@@ -1,3 +1,4 @@
+from PIL import Image
 import os
 from google_images_download import google_images_download
 
@@ -19,3 +20,8 @@ def download_image(joke):
     for asset in assets:
         if asset[-4:] == ".png":
             os.rename(f"assets/{asset}", "assets/image.png")
+
+    current_directory = os.getcwd()
+    image_directory = current_directory + "/assets/image.png"
+
+    image = Image.open(image_directory)
